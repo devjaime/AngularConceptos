42c31d9025557a3bbd0a6323b8ca32f6e2e36435
Jaime Hernandez <hernandez.hs@gmail.com>
2019-08-26 01:30:10 -0400

    cambios en componentes

--- Angular/hola-mundo/src/app/components/header/header.component.ts
+++ Angular/hola-mundo/src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 @Component({
     selector: 'app-header',
-    template: `<h1> Header Compoment</h1>`
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent { 
 
