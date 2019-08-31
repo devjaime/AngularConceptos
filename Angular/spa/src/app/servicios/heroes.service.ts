e6a5ee187b70bb90ea21737d4e983721e0e474dc
Jaime Hernandez <hernandez.hs@gmail.com>
2019-08-31 18:31:58 -0400

    se agregan más heroes para realizar prueba

--- Angular/spa/src/app/servicios/heroes.service.ts
+++ Angular/spa/src/app/servicios/heroes.service.ts
@@ -51,7 +51,36 @@ export class HeroesService {
           img: "assets/img/wolverine.png",
           aparicion: "1974-11-01",
           casa: "Marvel"
+        },
+        {
+          nombre: "IronMan",
+          bio: "Iron Man (también conocido en español como el Hombre de Hierro) es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. El personaje fue co-creado por el escritor y editor Stan Lee, desarrollado por el guionista Larry Lieber y diseñado por los artistas Don Heck y Jack Kirby. Hizo su primera aparición en Tales of Suspense # 39 (marzo de 1963), y recibió su propio título en Iron Man #1 (mayo de 1968).",
+          img: "assets/img/ironman.png",
+          aparicion: "1960-01-01",
+          casa: "Marvel"
+        },
+        {
+          nombre: "Thor",
+          bio: "Thor (Thor Odinson) es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. El personaje, que se basa en la deidad nórdica del mismo nombre, es el dios del trueno asgardiano que posee el martillo encantado, Mjolnir, que le otorga la capacidad de volar y manipular el clima entre sus otros atributos sobrehumanos.",
+          img: "assets/img/thor.png",
+          aparicion: "1960-01-01",
+          casa: "Marvel"
+        },
+        {
+          nombre: "Capitan America",
+          bio: "Capitán América cuyo nombre real es Steven  Rogers, es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por los historietistas Joe Simon y Jack Kirby, el personaje apareció por primera vez en Captain America Comics #1 (marzo de 1941) de Timely Comics, predecesor de Marvel Comics. El Capitán América fue diseñado como un supersoldado patriota que luchaba frecuentemente contra las potencias del Eje en la Segunda Guerra Mundial, y fue el personaje más popular de Timely Comics durante el período de guerra. La popularidad de los superhéroes se desvaneció después de la guerra, y el cómic del Capitán América dejó de editarse en 1950, con un breve resurgimiento en 1953. Desde que Marvel Comics revivió al personaje en 1964, el Capitán América se ha mantenido en publicación.",
+          img: "assets/img/capitanamerica.png",
+          aparicion: "1960-01-01",
+          casa: "Marvel"
+        },
+        {
+          nombre: "Avenger",
+          bio: "Los Vengadores (en inglés Avengers; en francés Les Vengeurs ; Invencibles del siglo XX en antiguas traducciones de México realizadas por Editorial La Prensa)1​2​ son un equipo ficticio de superhéroes que aparecen en cómics estadounidenses publicados por Marvel Comics. El equipo hizo su debut en The Avengers # 1 (septiembre de 1963),3​creado por el escritor y editor[cita requerida] Stan Lee y el artista/copiloto Jack Kirby. Los Vengadores es la renovación de Lee y Kirby de un equipo anterior de superhéroes, Escuadrón Todos los Ganadores, que apareció en la serie de cómics publicada por el predecesor de Marvel Comics, Timely Comics.",
+          img: "assets/img/avenger.png",
+          aparicion: "1960-01-01",
+          casa: "Marvel"
         }
+
       ];
 
     constructor() {
