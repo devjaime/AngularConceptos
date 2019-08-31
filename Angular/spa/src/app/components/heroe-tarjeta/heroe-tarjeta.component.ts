502ee7ce140f8f46a7ef6b65f00ccff1e748c8e4
Jaime Hernandez <hernandez.hs@gmail.com>
2019-08-31 17:41:27 -0400

    emitir un evento del hijo al padre

--- Angular/spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ Angular/spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,12 +10,18 @@ export class HeroeTarjetaComponent implements OnInit {
 
   @Input() heroe: any = {};
   @Input() index: number;
-  constructor(private router:Router) { }
+
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+  constructor(private router:Router) {
+    this.heroeSeleccionado = new EventEmitter();
+   }
 
   ngOnInit() {
   }
   verHeroe() {
     console.log(this.index);
-    this.router.navigate(['/heroe', this.index]);
+    // this.router.navigate(['/heroe', this.index]);
+    this.heroeSeleccionado.emit(this.index);
   }
 }
