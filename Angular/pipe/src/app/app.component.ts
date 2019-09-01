2b81cb41cd70c5a99cdebff58b17934820d349c3
Jaime Hernandez <hernandez.hs@gmail.com>
2019-09-01 01:03:43 -0400

    pipe uppercase, logwercase y slice

---
+++ Angular/pipe/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre = 'Jaime';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
