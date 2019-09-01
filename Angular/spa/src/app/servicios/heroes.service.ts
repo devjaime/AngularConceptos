ac43cdb3c742903a5e39e4930a386063ea906b3e
Jaime Hernandez <hernandez.hs@gmail.com>
2019-09-01 14:25:43 -0400

    cambios para que funcione con payasos

--- Angular/spa/src/app/servicios/heroes.service.ts
+++ Angular/spa/src/app/servicios/heroes.service.ts
@@ -4,82 +4,110 @@ import { Injectable } from '@angular/core';
 export class HeroesService {
     private heroes: Heroe[] = [
         {
-          nombre: "Aquaman",
-          bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
-          img: "assets/img/aquaman.png",
-          aparicion: "1941-11-01",
-          casa:"DC"
+          nombre: "IT (ESO) El Payaso",
+          bio: "It (titulada: It (Eso) en Hispanoamérica) es una película de 2017 producida por New Line Cinema, KatzSmith Productions, Lin Pictures y Vertigo Entertainment, y distribuida por Warner Bros. Pictures.8​ Es la segunda adaptación de la novela homónima de Stephen King y está destinada a ser la primera entrega de una duología planificada. La novela fue adaptada previamente en una miniserie de 1990.9​ La película cuenta la historia de siete niños en Derry, Maine, que son aterrorizados por un ser epónimo, solo para hacer frente a sus propios demonios personales en el proceso",
+          img: "assets/img/itelpayaso.png",
+          aparicion: "1988",
+          casa:"KING"
         },
         {
-          nombre: "Batman",
-          bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
-          img: "assets/img/batman.png",
-          aparicion: "1939-05-01",
-          casa:"DC"
+          nombre: "Killer Klowns from Outer Space",
+          bio: "Killer Klowns from Outer Space es una película de 1988 dirigida por Stephen Chiodo. Fue protagonizada por Grant Cramer, Suzanne Snyder, John Allen Nelson y Royal Dano. La trama gira en torno a un grupo de extraterrestres con apariencia de payasos que llegan al planeta Tierra para capturar y asesinar personas.",
+          img: "assets/img/evil-clown.png",
+          aparicion: "1988",
+          casa:"KILLER"
         },
         {
-          nombre: "Daredevil",
-          bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
-          img: "assets/img/daredevil.png",
-          aparicion: "1964-01-01",
-          casa: "Marvel"
+          nombre: "James McAvoy / Jaeden Lieberher (Bill Denbrough)",
+          bio: "El valiente líder del Club de los Perdedores regresa como un novelista muy similar a Stephen King. El actor de Fragmentado tomará la posta de Jaeden Lieberher, quien lo interpretó en la primera entrega.",
+          img: "assets/img/JamesMcAvoy.jpg",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "Hulk",
-          bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
-          img: "assets/img/hulk.png",
-          aparicion: "1962-05-01",
-          casa:"Marvel"
+          nombre: "Jessica Chastain / Sophia Lillis (Beverly Marsh)",
+          bio: "La actriz ya había trabajado con el realizador argentino Andy Muschietti en la película Mama. Chastain protagonizó el primer tráiler con el personaje que Sophia Lillis popularizó en la primera parte.",
+          img: "assets/img/JessicaChastain.jpg",
+          aparicion: "2019",
+          casa:"KING"
         },
         {
-          nombre: "Linterna Verde",
-          bio: "Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde (revisada por escritores posteriores como un poder místico llamado Starheart), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de lámparas llamado Chang)",
-          img: "assets/img/linterna-verde.png",
-          aparicion: "1940-06-01",
-          casa: "DC"
+          nombre: "James Ransone Jack Dylan Grazer / (Eddie Kaspbrak)",
+          bio: "Miembro del elenco en 'The Wire', Ransone ya cuenta con experiencia en el género de terror. Jack Dylan Grazer fue el primero en darle vida a Eddie, el hipocondriáco niño del primer capítulo de 'Eso'.",
+          img: "assets/img/JamesRansone.jpg",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "Spider-Man",
-          bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
-          img: "assets/img/spiderman.png",
-          aparicion: "1962-08-01",
-          casa: "Marvel"
+          nombre: "Andy Bean / Wyatt Oleff (Stan Uris)",
+          bio: "Uno de los personajes más afectados por el encuentro frente a Pennywise fue Stan Uris. Consumido por sus miedos, Andy Bean interpretará a una versión adulta de Wyatt Oleff dentro del grupo de perdedores.",
+          img: "assets/img/AndyBean.png",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "Wolverine",
-          bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
-          img: "assets/img/wolverine.png",
-          aparicion: "1974-11-01",
-          casa: "Marvel"
+          nombre: "Bill Hader / Finn Wolfhard (Richie Tozier)",
+          bio: "Finn Wolfhard y Bill Harder, ambos populares actores de Hollywood compartirán el rol de Richie Tozier en la segunda parte. El personaje se convierte en un conocido DJ en Beverly Hills luego de derrotar al payaso junto a sus amigos.",
+          img: "assets/img/BillHader.jpg",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "IronMan",
-          bio: "Iron Man (también conocido en español como el Hombre de Hierro) es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. El personaje fue co-creado por el escritor y editor Stan Lee, desarrollado por el guionista Larry Lieber y diseñado por los artistas Don Heck y Jack Kirby. Hizo su primera aparición en Tales of Suspense # 39 (marzo de 1963), y recibió su propio título en Iron Man #1 (mayo de 1968).",
-          img: "assets/img/ironman.png",
-          aparicion: "1960-01-01",
-          casa: "Marvel"
+          nombre: "Jay Ryan / Jeremy Ray Taylor (Ben Hanscom)",
+          bio: "Un gran cambio. Caracterizado por su voluminosa apariencia, Ben Hanscom será interpretado por el fornido Jay Ryan, quien toma el lugar de Jeremy Ray Taylor en la primera película. Este personaje forma una relación con Beverly luego del primer ataque.",
+          img: "assets/img/JayRyan.jpg",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "Thor",
-          bio: "Thor (Thor Odinson) es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. El personaje, que se basa en la deidad nórdica del mismo nombre, es el dios del trueno asgardiano que posee el martillo encantado, Mjolnir, que le otorga la capacidad de volar y manipular el clima entre sus otros atributos sobrehumanos.",
-          img: "assets/img/thor.png",
-          aparicion: "1960-01-01",
-          casa: "Marvel"
+          nombre: "Isaiah Mustafa / Mike Hanlon (Mike Hanlon)",
+          bio: "Mejor conocido como el ex portavoz de Old Spice, consiguió el papel de la versión adulta de Mike Hanlon. Curiosamente, su personaje es el único miembro del Club de Perdedores que se queda en Derry, el pueblo donde vive Pennywise.",
+          img: "assets/img/IsaiahMustafa.png",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "Capitan America",
-          bio: "Capitán América cuyo nombre real es Steven  Rogers, es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por los historietistas Joe Simon y Jack Kirby, el personaje apareció por primera vez en Captain America Comics #1 (marzo de 1941) de Timely Comics, predecesor de Marvel Comics. El Capitán América fue diseñado como un supersoldado patriota que luchaba frecuentemente contra las potencias del Eje en la Segunda Guerra Mundial, y fue el personaje más popular de Timely Comics durante el período de guerra. La popularidad de los superhéroes se desvaneció después de la guerra, y el cómic del Capitán América dejó de editarse en 1950, con un breve resurgimiento en 1953. Desde que Marvel Comics revivió al personaje en 1964, el Capitán América se ha mantenido en publicación.",
-          img: "assets/img/capitanamerica.png",
-          aparicion: "1960-01-01",
-          casa: "Marvel"
+          nombre: "Teach Grant / Nicholas Hamilton (Henry Bowers)",
+          bio: "Teach Grant no es tan conocido en el mundo de Hollywood y ha aparecido en papeles menores durante su carrera, pero tiene buenas habilidades de actuación según la crítica. En la piel del matón Henry Bowers, interpretado en un inicio por Nicholas Hamilton, tendrá como misión acabar con los protagonistas de una vez por todas.",
+          img: "assets/img/TeachGrant.png",
+          aparicion: "2019",
+          casa: "KING"
         },
         {
-          nombre: "Avenger",
-          bio: "Los Vengadores (en inglés Avengers; en francés Les Vengeurs ; Invencibles del siglo XX en antiguas traducciones de México realizadas por Editorial La Prensa)1​2​ son un equipo ficticio de superhéroes que aparecen en cómics estadounidenses publicados por Marvel Comics. El equipo hizo su debut en The Avengers # 1 (septiembre de 1963),3​creado por el escritor y editor[cita requerida] Stan Lee y el artista/copiloto Jack Kirby. Los Vengadores es la renovación de Lee y Kirby de un equipo anterior de superhéroes, Escuadrón Todos los Ganadores, que apareció en la serie de cómics publicada por el predecesor de Marvel Comics, Timely Comics.",
-          img: "assets/img/avenger.png",
-          aparicion: "1960-01-01",
-          casa: "Marvel"
-        }
+          nombre: "IT Segunda Parte",
+          bio: "La película contará con los actores James McAvoy, Jessica Chastain, Jay Ryan, Bill Hader, Isaiah Mustafa, James Ransone y Andy Bean en las versiones adultas de los protagonistas. Bill Skarsgård repite su papel de Pennywise, el payaso, junto con gran parte del elenco infantil del primer capítulo, incluidos Jaeden Lieberher, Sophia Lillis, Jeremy Ray Taylor, Finn Wolfhard, Chosen Jacobs, Jack Dylan Grazer, Wyatt Oleff y Jackson Robert Scott.",
+          img: "assets/img/ITSegundaParte.gif",
+          aparicion: "2019",
+          casa: "KING"
+        },
+        {
+          nombre: "Joker Cesar Romero",
+          bio: "La película contará con los actores James McAvoy, Jessica Chastain, Jay Ryan, Bill Hader, Isaiah Mustafa, James Ransone y Andy Bean en las versiones adultas de los protagonistas. Bill Skarsgård repite su papel de Pennywise, el payaso, junto con gran parte del elenco infantil del primer capítulo, incluidos Jaeden Lieberher, Sophia Lillis, Jeremy Ray Taylor, Finn Wolfhard, Chosen Jacobs, Jack Dylan Grazer, Wyatt Oleff y Jackson Robert Scott.",
+          img: "assets/img/cesarromero.jpg",
+          aparicion: "1960",
+          casa: "JOKER"
+        },
+        {
+          nombre: "Joker Jack Nicholson",
+          bio: "En la película de Batman (1989)  Joker fue el alias elegido por el criminal Jack Napier, al cual un accidente provocó que su rostro y su locura fueran alterados al caer en unos productos químicos. Convirtiéndose en el más grande enemigo de Batman, y los dos compartían un pasado que los hizo enemigos desde hace muchísimo tiempo atrás.",
+          img: "assets/img/JackNicholson.jpg",
+          aparicion: "1988",
+          casa: "JOKER"
+        },
+        {
+          nombre: "Joker Heath Ledger",
+          bio: "Este Joker como en los comics, no tiene un origen único. A lo largo de la cinta el personaje narra episodios de su vida, pero cada que cuenta una nueva historia se contradice con la anterior, lo que crea una duda en el espectador de que si lo que está diciendo es verdad. Su plan es introducir una anarquía en ciudad Gótica para desestabilizar sus órdenes sociales.",
+          img: "assets/img/HeathLedger.jpg",
+          aparicion: "2008",
+          casa: "JOKER"
+        },
+        {
+          nombre: "Joker Joaquin Phoenix",
+          bio: "Uno podría pensar que la vida de un actor está llena de glamour y sólo consiste en aprenderse e interpretar los textos, pero hay papeles que son un verdadero reto, ejemplo de ello es el sacrificio que hizo Joaquin Phoenix para interpretar al Joker. En el trailer más reciente puede verse la caracterización del actor como un comediante fracasado que poco a poco va transformándose en el psicópata criminal conocido como el ‘Joker’.",
+          img: "assets/img/juaquinphoenix.gif",
+          aparicion: "2019",
+          casa: "JOKER"
+       }
 
       ];
 
